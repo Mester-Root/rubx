8e45d4ff40e81bdf6a0411aeb11b9234228a6094
the server <106686224+Mester-Root@users.noreply.github.com>
2023-01-22 03:56:53 +0330

    Create anti_swear_words.py

---
+++ Tools/anti_swear_words.py
@@ -0,0 +1,28 @@
+from re import search
+
+finder = lambda text, filters: any(bool(search(string, text)) for string in filters)
+
+def parse(text: str, *args) -> bool:
+  '''
+  return an bool to detecting badwords in text
+  '''
+  return finder(text, ['bad', 'swear', 'unlike', 'fuck'])
+
+print(parse('Hello bad and dad fucker'))
+
+# Examples
+
+'''
+from rb import Handler, Filters, Performers
+
+client = Handler('session')
+
+def event(update):
+    if parse(update.message.text):
+        message.delete()
+        # message.delete([update.message.message_id], update.group_guid)
+
+client.add_event_handling(Performers.hand_shake, event=dict(get_chats=False, get_messages=True))
+client.start = True
+client.command_handler(event)
+'''
