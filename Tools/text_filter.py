7bba6f9c8661f08606eaf6d930d712a964f9b50d
the server <106686224+Mester-Root@users.noreply.github.com>
2023-01-24 19:27:07 +0330

    Update text_filter.py

--- Tools/text_filter.py
+++ Tools/text_filter.py
@@ -3,11 +3,11 @@
 import re, typing
 
 def detecting(__text: str, __inappropriate_words: list = ['bad', 'evil', 'horrible', 'awful', 'fuck', 'bitch', 'unlike'], __replace_to_char: typing.Union[str, list] = '-*~') -> tuple:
-    search: bool = False
+    search, replace_to_char = False,  __import__('random').choice(__replace_to_char)
     for word in __inappropriate_words: 
         find = re.compile(word).search(__text, re.IGNORECASE)
         if find:
-            __text, search = re.sub(re.compile(__text[find.span()[0]:find.span()[1]]), ''.join(list(map(lambda i: __import__('random').choice(__replace_to_char), range(len(word))))), __text, 1), True
+            __text, search = re.sub(re.compile(__text[find.span()[0]:find.span()[1]]), ''.join(list(map(lambda i: replace_to_char, range(len(word))))), __text, 1), True
     else:
         return search, __text
 
