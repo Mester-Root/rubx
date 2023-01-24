7e395ec265bbb48e5166b8564c42f6ed1a0aa4e4
the server <106686224+Mester-Root@users.noreply.github.com>
2023-01-24 19:21:26 +0330

    Create porn_detection.py

---
+++ Tools/porn_detection.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import cv2, joblib, numpy as np
+#from sklearn.externals import * 
+
+def detection(image: str) -> tuple:
+    
+    clf = joblib.load('porn_classifier.pkl')
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    resized_image = cv2.resize(gray, (200, 200))
+    flattened_image = resized_image.flatten()  
+    
+    if (clf.predict([flattened_image]) == 'porn'):
+        return True, 'this image contains porn content'
+    else:  
+        return False, 'this image does not contain porn content'
+
+print('*.jpg')
